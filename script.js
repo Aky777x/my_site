883b3cf01410027fa95bcd3ae341fe030149b532
Aky777x <l.o.s.t.m.y.m.i.n.dxxx@gmail.com>
2021-12-06 22:18:49 +0900

    test

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
 (function () {
+  /* デバイスの確認※iOSかどうかを判定したい */
   const ua = navigator.userAgent;
-  console.log("端末情報: " + ua);
-  debugger;
-  if (ua.indexOf("iPhone") > 0 || ua.indexOf("iPad") > 0) {
-    // iOS用処理 ※iOSはバグや特殊な仕様があるので、処理を分ける
+  // iOS用処理 ※iOSはバグや特殊な仕様があるので、処理を分ける
+  const iosFlg = ua.indexOf("iPhone") > 0 || ua.indexOf("iPad") > 0;
 
+  if (iosFlg) {
     document.getElementById("kv-wrap").style.height = "80vh";
     document.getElementsByClassName("site-name-wrap")[0].style.height = "80vh";
-  } else {
-    // PC用処理
   }
+
+  function delayScrollAnime() {
+    var time = 0.2; //遅延時間を増やす秒数の値
+    var value = time;
+    $(".delayScroll").each(function () {
+      var parent = this; //親要素を取得
+      var elemPos = $(this).offset().top; //要素の位置まで来たら
+      var scroll = $(window).scrollTop(); //スクロール値を取得
+      if (iosFlg) {
+        scroll += 300; //iOS端末の場合、スクロールの判定を操作する
+      }
+      var windowHeight = $(window).height(); //画面の高さを取得
+      var childs = $(this).children(); //子要素を取得
+
+      if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {
+        //指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+        $(childs).each(function () {
+          JSON.stringify("親: " + parent);
+          JSON.stringify("子: " + childs);
+          if (!$(this).hasClass("fadeUp")) {
+            //アニメーションのクラス名が指定されているかどうかをチェック
+
+            $(parent).addClass("play"); //親要素にクラス名playを追加
+            console.log("対象: " + $(this));
+            console.log("対象: " + this);
+            $(this).css("animation-delay", value + "s"); //アニメーション遅延のCSS animation-delayを追加し
+            $(this).addClass("fadeUp"); //アニメーションのクラス名を追加
+            value = value + time; //delay時間を増加させる
+
+            //全ての処理を終わったらplayを外す
+            var index = $(childs).index(this);
+            if (childs.length - 1 == index) {
+              $(parent).removeClass("play");
+            }
+          }
+        });
+      } else {
+        $(childs).removeClass("fadeUp"); //アニメーションのクラス名を削除
+        value = time; //delay初期値の数値に戻す
+      }
+    });
+  }
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
+  }); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on("load", function () {
+    delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
+  }); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
   const width = new Vue({
     el: ".width",
